376428d447aa98fdcd2534625f02351bad223490
David <a85233@ualg.pt>
2026-05-17 20:05:58 +0100

    adicao do ficheiro de autenticacao

---
+++ lib/data/auth_servico.dart
@@ -0,0 +1,35 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  // LOGIN COM GOOGLE (OAuth2)
+  Future<User?> loginWithGoogle() async {
+    final GoogleSignInAccount? googleUser =
+        await GoogleSignIn().signIn();
+
+    if (googleUser == null) return null;
+
+    final googleAuth = await googleUser.authentication;
+
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+
+    final userCredential =
+        await _auth.signInWithCredential(credential);
+
+    return userCredential.user;
+  }
+
+  // LOGOUT
+  Future<void> logout() async {
+    await GoogleSignIn().signOut();
+    await _auth.signOut();
+  }
+
+  // UTILIZADOR ATUAL
+  User? get currentUser => _auth.currentUser;
+}
